7cff9dc1197f4aa37f1a7af4c2a4ef37feaa89ab
Yazmin Trucido <69005528+YazminTrucido@users.noreply.github.com>
2024-02-18 22:34:02 -0300

    Add files via upload

---
+++ birthdays/birthdays.db.sql
@@ -0,0 +1,28 @@
+----
+-- phpLiteAdmin database dump (https://www.phpliteadmin.org/)
+-- phpLiteAdmin version: 1.9.9-dev
+-- Exported: 10:32pm on February 18, 2024 (-03)
+-- database file: /workspaces/69005528/birthdays/birthdays.db
+----
+BEGIN TRANSACTION;
+
+----
+-- Table structure for birthdays
+----
+CREATE TABLE birthdays (
+    id INTEGER,
+    name TEXT,
+    month INTEGER,
+    day INTEGER,
+    PRIMARY KEY(id)
+);
+
+----
+-- Data dump for birthdays, a total of 5 rows
+----
+INSERT INTO "birthdays" ("id","name","month","day") VALUES ('1','Harry','7','31');
+INSERT INTO "birthdays" ("id","name","month","day") VALUES ('2','Ron','3','1');
+INSERT INTO "birthdays" ("id","name","month","day") VALUES ('3','Hermione','9','19');
+INSERT INTO "birthdays" ("id","name","month","day") VALUES ('4','Yazmin','8','16');
+INSERT INTO "birthdays" ("id","name","month","day") VALUES ('5','Leandro','8','16');
+COMMIT;
